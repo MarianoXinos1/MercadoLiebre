fb88d99b3d2a4c844ef288cfdb5a80624d804afc
MarianoXinos1 <mxinos@hotmail.com>
2024-02-27 15:24:12 -0300

    Limpieza rutas App.jss

--- app.js
+++ app.js
@@ -15,14 +15,10 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")
 
 // Formulario html de Crea tu Cuenta
 app.get("/register", (req,res) => res.sendFile(path.join(__dirname,"/views/register.html")));
-app.post("/register", (req, res) => res.sendFile(path.join(__dirname, "/")));
-
-app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")));
-
 
 // Formulario html de login
 app.get("/login", (req,res) => res.sendFile(path.join(__dirname,"/views/login.html")));
-app.post("/login", (req, res) =>  res.redirect("/home.html"));
+
 
 
 
