0773b13354715dbfe4ee6258d311fa609007e2be
MarianoXinos1 <mxinos@hotmail.com>
2024-02-26 21:56:00 -0300

    Practica Individual De MercadoLiebre

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const path = require("path");
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log ('servidor corriendo'));
+
+// Para mi LocalHost
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")));
+
+// Formulario html de Crea tu Cuenta
+app.get("/register", (req,res) => res.sendFile(path.join(__dirname,"/views/register.html")));
+app.post("/register", (req, res) => res.sendFile(path.join(__dirname, "/")));
+
+app.get("/home", (req, res) => res.sendFile(path.join(__dirname, "/views/home.html")));
+
+
+// Formulario html de login
+app.get("/login", (req,res) => res.sendFile(path.join(__dirname,"/views/login.html")));
+app.post("/login", (req, res) =>  res.redirect("/home.html"));
+
+
+
